7cb455edc7872571fd83172242b9e002236a29ff
heardMan <markyheard@gmail.com>
2019-02-16 09:50:37 -0800

    fixing some bugs

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,11 @@ CREATE database burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  ID int NOT NULL AUTO_INCREMENT,
+  ID int AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(255) NOT NULL,
   devoured BOOLEAN NOT NULL,
   img int NOT NULL,
+  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (ID)
 );
 
